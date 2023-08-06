67baa208fc193eda1f426713771450d043e60f31
Udayan Shevade <udayan.shevade@gmail.com>
2023-08-06 10:55:51 -0700

    add dotenv config, refactor db setup

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
-from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask import Flask
 from flask_cors import CORS
+from models import setup_db
 
 def create_app(test_config=None):
   # create and configure the app
   _app = Flask(__name__)
-  _app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/castingagency'
+  setup_db(_app)
   CORS(_app)
 
   return _app
 
 app = create_app()
-
-db = SQLAlchemy(app)
-
-migrate = Migrate(app, db)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
--- models.py
+++ models.py
@@ -1,4 +1,22 @@
-from app import db
+from sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import dotenv_values
+
+db = SQLAlchemy()
+
+config = dotenv_values(".env")
+
+database_host = config['DATABASE_HOST']
+database_name = config['DATABASE_NAME']
+database_path = "postgresql://{}/{}".format(database_host, database_name)
+
+def setup_db(app, database_path = database_path):
+  app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+  db.app = app
+  db.init_app(app)
+  db.create_all()
+
+migrate = Migrate(db.app, db)
 
 class Movie(db.Model):
   __tablename__ = "movies"
