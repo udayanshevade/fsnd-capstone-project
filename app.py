6a4ca0752d12a6a28c7368043c44765e7d199f14
Udayan Shevade <udayan.shevade@gmail.com>
2023-08-06 09:54:49 -0700

    init app

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
 
 def create_app(test_config=None):
   # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+  _app = Flask(__name__)
+  _app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost:5432/castingagency'
+  CORS(_app)
 
-  return app
+  return _app
 
-APP = create_app()
+app = create_app()
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app, db)
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
