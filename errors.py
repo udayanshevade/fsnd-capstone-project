50c9d965aea9699e7eaf3dc820837bc147ddc067
Udayan Shevade <udayan.shevade@gmail.com>
2023-08-13 08:28:52 -0700

    add 500 code server error handling

--- errors.py
+++ errors.py
@@ -20,7 +20,6 @@ def app_error_handling(app):
             "message": "unprocessable"
         }), 422
 
-
     @app.errorhandler(404)
     def unreachable(self):
         return jsonify({
@@ -29,6 +28,13 @@ def app_error_handling(app):
             'message': 'unreachable'
         }), 404
 
+    @app.errorhandler(500)
+    def internal_error(self):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'internal_server_error'
+        })
 
     @app.errorhandler(AuthError)
     def handle_auth_error(error: AuthError):
